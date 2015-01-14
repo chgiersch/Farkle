55dc25d53a6c8be3a9c8aa1ad9d1141565e3dcd9
Chris Giersch <chgiersch@gmail.com>
2015-01-14 14:16:32 -0800

    Step 2 DONE - tap gesture works

---
+++ Farkle/DieLabel.h
@@ -0,0 +1,13 @@
+//
+//  DieLabel.h
+//  Farkle
+//
+//  Created by Chris Giersch on 1/14/15.
+//  Copyright (c) 2015 ChrisGiersch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DieLabel : UILabel
+
+@end
---
+++ Farkle/DieLabel.m
@@ -0,0 +1,27 @@
+//
+//  DieLabel.m
+//  Farkle
+//
+//  Created by Chris Giersch on 1/14/15.
+//  Copyright (c) 2015 ChrisGiersch. All rights reserved.
+//
+
+#import "DieLabel.h"
+#import <UIKit/UIKit.h>
+
+@implementation DieLabel
+
+-(IBAction)onTapped:(UITapGestureRecognizer *)sender
+{
+    NSLog(@"sdfsf");
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
